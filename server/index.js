8af3eb5f69b7687e2d9a9c2eb505cf3252aa2ebe
Viktor Sivulskiy <info@softmaker.kz>
2022-04-29 16:55:44 +0600

    Server connection was established

--- server/index.js
+++ server/index.js
@@ -8,3 +8,10 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://so5tmaker:so5tmaker123@cluster0.nygjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
