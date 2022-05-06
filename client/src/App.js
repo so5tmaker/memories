6825b6942faddb1a6d7a8280db475d8b2f490b81
Viktor Sivulskiy <info@softmaker.kz>
2022-05-06 14:47:47 +0600

    Layout sctructure was added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.jpeg';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container just="space-between" alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sn={7}>
+                            <Post />
+                        </Grid>
+                        <Grid item xs={12} sn={4}>
+                            <Form></Form>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
