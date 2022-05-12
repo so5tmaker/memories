80496adf3319ececb2b7662d9cca31170be46ae5
Viktor Sivulskiy <info@softmaker.kz>
2022-05-12 08:33:46 +0600

    Form and Posts were added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from './images/memories.jpeg';
 
 const App = () => {
@@ -14,7 +16,7 @@ const App = () => {
                 <Container>
                     <Grid container just="space-between" alignItems='stretch' spacing={3}>
                         <Grid item xs={12} sn={7}>
-                            <Post />
+                            <Posts />
                         </Grid>
                         <Grid item xs={12} sn={4}>
                             <Form></Form>
